2e535328712aa518f9ec70b23bc691a1ba5801c9
Derricko Swink <dswinkgfx@gmail.com>
2024-06-29 07:48:29 -0400

    Add files via upload

--- CreditScoreGrade.java
+++ CreditScoreGrade.java
@@ -31,6 +31,7 @@ public class CreditScoreGrade {
 		if (creditScore >= 300 && creditScore <= 620) {
 			System.out.println("\nCredit Grade: D");
 			System.out.println("\nRange: BELOW AVERAGE / Average / Good / Excellent");
+			System.out.println("\nStar Rating: *");
 			System.out.println("\nYou are " + excellentCredit + " points away from having excellent credit!");
 			System.out.println("\nDon't give up now... stay focused!");
 		} 
@@ -38,12 +39,14 @@ public class CreditScoreGrade {
 		else if (creditScore >= 621 && creditScore <= 700) {
 			System.out.println("\nCredit Grade: C");
 			System.out.println("\nRange: Below Average / AVERAGE / Good / Excellent");
+			System.out.println("\nStar Rating: **");
 			System.out.println("\nYou are " + excellentCredit + " points away from having excellent credit!");
 			System.out.println("\nIt's too late to turn back now... keep pushing!");
 		}
 		else if (creditScore >= 701 && creditScore <= 760) {
 			System.out.println("\nCredit Grade: B");
 			System.out.println("\nRange: Below Average / Average / GOOD / Excellent");
+			System.out.println("\nStar Rating: ***");
 			System.out.println("\nYou are only " + excellentCredit + " points away from having excellent credit!");
 			System.out.println("\nYou're almost there... keep going!");
 		} 
@@ -51,6 +54,7 @@ public class CreditScoreGrade {
 		else if (creditScore >= 761 && creditScore <= 850) {
 			System.out.println("\nCredit Grade: A+");
 			System.out.println("\nRange: Below Average / Average / Good / EXCELLENT");
+			System.out.println("\nStar Rating: ****");
 			System.out.println("\nGreat job!");
 		}
 		
